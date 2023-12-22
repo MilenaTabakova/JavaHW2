21514b86cab36a765a8695376358d6edaee35250
Milena Tabakova <milena-lugowaja@yandex.ru>
2023-12-22 21:20:24 +0300

    add Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int price = 6000;
+        int bonus = 20;
+
+        System.out.println("Количество бонусных миль");
+        System.out.println((price / bonus + " миль"));
+
+
 
 
 
